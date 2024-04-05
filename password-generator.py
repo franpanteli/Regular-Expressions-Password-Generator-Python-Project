f101359492a15b4d7cca758bb6cc2d73dd8a5923
Fran Panteli <francescapanteli@gmail.com>
2024-04-05 18:31:28 +0100

    This commit pushes changes to the project password generator .py file.

---
+++ password-generator.py
@@ -0,0 +1,40 @@
+import re
+import secrets
+import string
+
+
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        constraints = [
+            (nums, r'\d'),
+            (lowercase, r'[a-z]'),
+            (uppercase, r'[A-Z]'),
+            (special_chars, fr'[{symbols}]')
+        ]        
+
+        # Check constraints
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            return password
+if __name__=='__main__' :
+    new_password = generate_password()
+    print('Generated Password:', new_password)
+#pattern = 'r\W'
+#quote = ("_")
+#print(re.findall(pattern, quote))
+
+
